096f6280c66833dbfd701fbe8776d69bf091babe
воробйов александр <avorobeov21@gmail.com>
2023-05-24 11:05:39 +0300

    fix

--- Homework_number_43/Program.cs
+++ Homework_number_43/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Homework_number_43
 {
